642cf8a5a885647654f7fbc0b665de46d559c0b3
napoknot21 <napoknot21@protonmail.com>
2025-05-15 16:58:36 +0200

    Restart notebook.py initial version

--- src/notebook.py
+++ src/notebook.py
@@ -7,15 +7,15 @@ from turbulence.train import train
 nb_images = 500 # Maximum = 500
 val_size = 0.2
 test_size = 0.2
-batch_size = 16
+batch_size = 4
 
 # Training parameters
-num_training_epochs = 50#1250
-pretrained = True
-num_pretrained_epochs = 1250
+num_training_epochs = 1250
+pretrained = False
+num_pretrained_epochs = 0
 
 image_folder_path = './velocity_images'
 train_loader, val_loader, test_loader = load_data(image_folder_path, nb_images, val_size, test_size, batch_size)
 
 pretrained_model_path = f'./turbulence/pretrained/turbulence_epoch_{num_pretrained_epochs}.ckpt'
-train(pretrained_model_path, train_loader, val_loader, epochs=num_training_epochs, pretrained=pretrained, save_name="lpips")
+train(pretrained_model_path, train_loader, val_loader, epochs=num_training_epochs, pretrained=pretrained)
